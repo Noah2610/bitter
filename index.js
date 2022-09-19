9bac2a392c4e6607dbc40588a2ac532315bd62e1
Noah Rosenzweig <rosenzweig.noah@gmail.com>
2022-09-19 13:18:09 +0200

    Setup mock twitter clone

---
+++ index.js
@@ -0,0 +1,18 @@
+const POSTS = [
+    {
+        author: {
+            name: "Linus Torvalds",
+            avatar: null,
+        },
+        post: `Change my mind: All software should
+support Linux, and companies that
+don't can eff-off!`,
+    },
+    {
+        author: {
+            name: "Richard Stallman",
+            avatar: null,
+        },
+        post: "I'm a GNUru!",
+    },
+];
